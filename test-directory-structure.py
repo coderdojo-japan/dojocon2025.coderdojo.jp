46552b1314725e8581983e1b8150bd8796e16348
mucho613 <mucho613@users.noreply.github.com>
2025-08-29 16:19:06 +0000

    deploy: ff665bbe74195c7f5bf933e16d09ac47c00e1694

--- test-directory-structure.py
+++ test-directory-structure.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python
+from html.parser import HTMLParser
 from pathlib import Path
 
 """
@@ -10,6 +11,39 @@ foobar/baz/index.html
 foobar/baz.html
 -> /foobar/baz/ がルーティングできない
 """
+
+
+class Parser(HTMLParser):
+    """
+    <meta http-equiv="refresh"> を含むファイルを検査の対象から除外する
+    """
+
+    def __init__(self, *, convert_charrefs: bool = True) -> None:
+        super().__init__(convert_charrefs=convert_charrefs)
+        self.is_redirect = False
+
+    def handle_starttag(self, tag: str, attrs: list[tuple[str, str | None]]) -> None:
+        if self.is_redirect:
+            return
+
+        if tag != "meta":
+            return
+
+        for attr_name, attr_value in attrs:
+            if attr_name == "http-equiv" and attr_value == "refresh":
+                self.is_redirect = True
+                break
+
+
+def is_redirect(path: str) -> bool:
+    with open(path) as fp:
+        content = fp.read()
+
+    parser = Parser()
+    parser.feed(content)
+    parser.close()
+
+    return parser.is_redirect
 
 
 def main() -> int:
@@ -25,6 +59,9 @@ def main() -> int:
                 continue
 
             path = cwd.joinpath(file).as_posix()
+            if is_redirect(path):
+                continue
+
             errors.append(path)
 
     if 0 < len(errors):
